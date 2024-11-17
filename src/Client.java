3851c21a6debc2981a35621dffa71d3b9b56fdc6
vedavedaveda <veda@ruc.dk>
2024-11-17 17:43:09 +0100

    Console interaction added

--- src/Client.java
+++ src/Client.java
@@ -2,13 +2,56 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) throws NotBoundException, MalformedURLException, RemoteException
-    {
-        PrintServerI printServer = (PrintServerI) Naming.lookup("rmi://localhost:5099/print");
-        System.out.println(printServer.print("Hello", "printer 2"));
+    public static void main(String[] args) throws NotBoundException, MalformedURLException, RemoteException {
+        try {
+            PrintServerI printServer = (PrintServerI) Naming.lookup("rmi://localhost:5099/print");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Connected to print server.");
+
+            while(true) {
+                System.out.print("Enter command: ");
+                String command = scanner.nextLine();
+
+                switch (command) {
+                    case "print":
+                        System.out.println(printServer.print("hello.file", "Printer 3"));
+                        break;
+                    case "queue":
+                        System.out.println("Command not yet functional");
+                        break;
+                    case "topQueue":
+                        System.out.println("Command not yet functional");
+                        break;
+                    case "start":
+                        System.out.println("Command not yet functional");
+                        break;
+                    case "stop":
+                        System.out.println("Command not yet functional");
+                        break;
+                    case "restart":
+                        System.out.println("Command not yet functional");
+                        break;
+                    case "status":
+                        System.out.println("Command not yet functional");
+                        break;
+                    case "readConfig":
+                        System.out.println("Command not yet functional");
+                        break;
+                    case "setConfig":
+                        System.out.println("Command not yet functional");
+                        break;
+                    case "exit":
+                        System.out.println("Exiting...");
+                        return;
+                    default:
+                        System.out.println("Invalid command.");
+                }
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
-
-
 }
--- src/PrintServer.java
+++ src/PrintServer.java
@@ -2,17 +2,17 @@ import java.rmi.server.UnicastRemoteObject;
 import java.rmi.RemoteException;
 
 public class PrintServer extends UnicastRemoteObject implements PrintServerI {
-
     public PrintServer() throws RemoteException {
         super();
     }
 
+
     public String print(String filename, String printer) throws RemoteException {
-        return "Printing...";
+        return "Printing file: " + filename + " on printer: " + printer;
     }
 
     public String queue(String printer) throws RemoteException {
-        return printer;
+        return "wow.";
     }
 
     public void topQueue(String printer, int job) throws RemoteException {
